359896745c56bf1509985986df96e4b083944e45
IsenfireLDC <isenfireldc@gmail.com>
2021-04-06 11:47:09 -0500

    Adds new library components

---
+++ components/lib/port.vhd
@@ -0,0 +1,27 @@
+library ieee;
+use ieee.std_logic_1164.all;
+
+package port_pkg is
+	type port8_t is record
+		clk, rst: std_logic;
+		address: std_logic_vector(0 downto 0);
+		byteenable: std_logic_vector(0 downto 0);
+		write, read: std_logic;
+	end record;
+	type port16_t is record
+		clk, rst: std_logic;
+		address: std_logic_vector(0 downto 0);
+		byteenable: std_logic_vector(1 downto 0);
+		write, read: std_logic;
+	end record;
+	type port32_t is record
+		clk, rst: std_logic;
+		address: std_logic_vector(0 downto 0);
+		byteenable: std_logic_vector(3 downto 0);
+		write, read: std_logic;
+	end record;
+
+	subtype bus8_t is std_logic_vector(7 downto 0);
+	subtype bus16_t is std_logic_vector(15 downto 0);
+	subtype bus32_t is std_logic_vector(31 downto 0);
+end package;
---
+++ components/lib/port_reg.vhd
@@ -0,0 +1,46 @@
+library ieee;
+use ieee.std_logic_1164.all;
+
+entity port_reg is
+	generic(n: integer := 32);
+	port(
+		clk, rst: in std_logic;
+		read, write: in std_logic;
+		iread, iwrite: in std_logic_vector(n-1 downto 0);
+		oread, owrite: out std_logic_vector(n-1 downto 0)
+	);
+end entity;
+
+architecture arch of port_reg is
+	signal readdata, writedata: std_logic_vector(n-1 downto 0);
+	begin
+		--Component output
+		pregread: process(all)
+		begin
+			if rising_edge(clk) then
+				if rst = '1' then
+					readdata <= (others => '0');
+				else
+					readdata <= iread;
+				end if;
+			end if;
+		end process;
+
+		with read select oread <=
+			readdata when '1',
+			(others => '0') when others;
+
+		--Component input
+		pregwrite: process(all)
+		begin
+			if rising_edge(clk) then
+				if rst = '1' then
+					writedata <= (others => '0');
+				elsif write = '1' then
+					writedata <= iwrite;
+				end if;
+			end if;
+		end process;
+
+		owrite <= writedata;
+end architecture;
