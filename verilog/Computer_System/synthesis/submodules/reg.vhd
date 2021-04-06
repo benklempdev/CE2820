7ae3e17d8cbf6f15992aa53aff1acfb6e42b4967
IsenfireLDC <isenfireldc@gmail.com>
2021-04-06 12:01:27 -0500

    Adds generated files

---
+++ verilog/Computer_System/synthesis/submodules/reg.vhd
@@ -0,0 +1,28 @@
+library ieee;
+use ieee.std_logic_1164.all;
+
+--register (default 32-bit)
+--active low load
+--active low asyncronous reset
+entity reg is
+	generic (n : integer := 32);
+	port(
+		ld, rst, clk: in std_logic;
+		d: in std_logic_vector(n-1 downto 0);
+		q: out std_logic_vector(n-1 downto 0)
+	);
+end entity;
+
+architecture arch of reg is
+	begin
+		preg: process(all)
+			begin
+				if rst='0' then
+					q <= (others => '0');
+				elsif rising_edge(clk) then
+					if ld='0' then
+						q <= d;
+					end if;
+				end if;
+		end process;
+end architecture;
---
+++ verilog/Computer_System/synthesis/submodules/servo_control_port.vhd
@@ -0,0 +1,32 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use work.port_pkg.all;
+
+entity servo_control_port is
+	port(
+		avalon: in port16_t;
+		writedata: in bus16_t;
+		readdata: out bus16_t
+	);
+end entity;
+
+architecture arch of servo_control_port is
+	signal en, rst, clk: std_logic;
+	signal pos: std_logic_vector(7 downto 0);
+	signal control: std_logic;
+
+	signal data: std_logic_vector(15 downto 0);
+	begin
+		--Map signals
+		rst <= avalon.rst;
+		clk <= avalon.clk;
+		en <= data(8);
+		pos <= data(7 downto 0);
+
+		--Port registers
+		cportreg: entity work.port_reg generic map(n => 16)
+		port map(clk, rst, avalon.read, avalon.write, data, writedata, readdata, data);
+
+		--servo_control component
+		csrvctrl: entity work.servo_control port map(en, rst, clk, pos, control);
+end architecture;
