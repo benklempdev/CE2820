d4f71c8a5fcab50988c310877e0d6256d0e5a652
IsenfireLDC <isenfireldc@gmail.com>
2021-04-20 11:39:12 -0500

    Stopped using custom type for port

--- components/lib/servo_control_port.vhd
+++ components/lib/servo_control_port.vhd
@@ -5,7 +5,10 @@ use work.port_pkg.all;
 
 entity servo_control_port is
 	port(
-		avalon: in port16_t;
+		clk, rst: in std_logic;
+		address : in std_logic_vector(0 downto 0);
+		byteenable: in std_logic_vector(1 downto 0);
+		write, read: in std_logic;
 		writedata: in bus16_t;
 		readdata: out bus16_t;
 		control: out std_logic
@@ -13,7 +16,7 @@ entity servo_control_port is
 end entity;
 
 architecture arch of servo_control_port is
-	signal en, rst, clk: std_logic;
+	signal en: std_logic;
 	signal pos: std_logic_vector(7 downto 0);
 
 	signal data: std_logic_vector(15 downto 0);
@@ -26,7 +29,7 @@ architecture arch of servo_control_port is
 
 		--Port registers
 		cportreg: entity work.port_reg generic map(n => 16)
-		port map(clk, rst, avalon.read, avalon.write, data, writedata, readdata, data);
+		port map(clk, rst, read, write, data, writedata, readdata, data);
 
 		--servo_control component
 		csrvctrl: entity work.servo_control port map(en, rst, clk, pos, control);
